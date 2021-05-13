96a283c78b1dec361e68f5d7e743723a7c23e0d6
Patricia Batalha <patibatalha@gmail.com>
2021-05-13 17:11:41 -0300

    Initial Commit

---
+++ src/main/java/com/wishlist/wishlistluizacode/WishlistLuizacodeApplication.java
@@ -0,0 +1,13 @@
+package com.wishlist.wishlistluizacode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistLuizacodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistLuizacodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wishlist/wishlistluizacode/WishlistLuizacodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wishlist.wishlistluizacode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishlistLuizacodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
