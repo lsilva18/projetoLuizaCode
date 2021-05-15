1a7677cc4440a8b9679756ec983a36d56b0a72de
Patricia Batalha <patibatalha@gmail.com>
2021-05-15 15:41:32 -0300

    WishList initial implementations.

--- src/main/java/com/wishlist/wishlistluizacode/services/WishListService.java
+++ src/main/java/com/wishlist/wishlistluizacode/services/WishListService.java
@@ -1,17 +1,44 @@
 package com.wishlist.wishlistluizacode.services;
 
+import com.wishlist.wishlistluizacode.entities.Client;
+import com.wishlist.wishlistluizacode.entities.Product;
+import com.wishlist.wishlistluizacode.entities.WishList;
+import com.wishlist.wishlistluizacode.repositories.ProductRepository;
+import com.wishlist.wishlistluizacode.repositories.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WishListService {
 
-    // Save wishlist
+    @Autowired
+    private WishListRepository wishListRepository;
 
-    // Delete wishlist
+    // Add wishlist
+    public WishList save(WishList wishList){
+        return wishListRepository.save(wishList);
+    }
 
-    // Find all wishlists
+    // Find all
+    public List<WishList> findAll(){
+        Iterable<WishList> iterable = wishListRepository.findAll();
+        List<WishList> wishLists = new ArrayList<>();
+        iterable.forEach(wishLists::add);
+        return wishLists;
+    }
 
-    // Find wishlist by client name
+    // Find by name
+//    public List<Client> findById(String name){
+//        return wishListRepository.findByName(name);
+//    }
+
+    // Delete
+    public void delete(WishList wishList){
+        wishListRepository.delete(wishList);
+    }
 
     // Save product
 
