0804ac265154ebd41b6b13aa8852293644aaefcf
lara_rezende <lara_milena7@hotmail.com>
2021-05-16 14:42:51 -0300

    cliente controler

--- src/main/java/com/wishlist/wishlistluizacode/controllers/ClientController.java
+++ src/main/java/com/wishlist/wishlistluizacode/controllers/ClientController.java
@@ -1,4 +1,47 @@
 package com.wishlist.wishlistluizacode.controllers;
 
 public class ClientController {
+
+    // Add client
+    @Autowired
+    private ClientService clientService;
+
+    @PostMapping("/client")
+    public Client saveClient(@RequestBody Client client){
+        return clientService.save(client);
+    }
+
+    // Find all
+
+
+
+    // Find by name
+    @GetMapping("/client/{name}")
+    public Client getClient(@PathVariable(value = "name") String name){
+        return clienteService.get(name);
+    }
+
+    // Delete
+
+    @RequestMapping( value=delete/{id}, method = RequestMethod.POST)
+    public void delete(@RequestParam("id")Long id) {
+
+        Convidado client = clientRepository.findOne(id);
+
+        clientRepository.delete(client);
+
+    }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 }
