0d22f95f33f439ab694b0c18590ad7210497d32f
Patricia Batalha <patibatalha@gmail.com>
2021-05-16 14:28:09 -0300

    Wishlist service implementation

--- src/main/java/com/wishlist/wishlistluizacode/services/WishListService.java
+++ src/main/java/com/wishlist/wishlistluizacode/services/WishListService.java
@@ -1,15 +1,14 @@
 package com.wishlist.wishlistluizacode.services;
 
 import com.wishlist.wishlistluizacode.entities.Client;
-import com.wishlist.wishlistluizacode.entities.Product;
 import com.wishlist.wishlistluizacode.entities.WishList;
-import com.wishlist.wishlistluizacode.repositories.ProductRepository;
 import com.wishlist.wishlistluizacode.repositories.WishListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class WishListService {
@@ -17,35 +16,46 @@ public class WishListService {
     @Autowired
     private WishListRepository wishListRepository;
 
-    // Add wishlist
-    public WishList save(WishList wishList){
+    @Autowired
+    private ClientService clientService;
+
+    public WishList save(WishList wishList) {
         return wishListRepository.save(wishList);
     }
 
-    // Find all
-    public List<WishList> findAll(){
+    public List<WishList> findAll() {
         Iterable<WishList> iterable = wishListRepository.findAll();
         List<WishList> wishLists = new ArrayList<>();
         iterable.forEach(wishLists::add);
         return wishLists;
     }
 
-    // Find by name
-//    public List<Client> findById(String name){
-//        return wishListRepository.findByName(name);
-//    }
-
-    // Delete
-    public void delete(WishList wishList){
-        wishListRepository.delete(wishList);
+    public Optional<WishList> findById(Long id) {
+        return wishListRepository.findById(id);
     }
 
-    // Save product
+    public List<WishList> findByClientName(String name) {
+        List<Client> clients = clientService.findByName(name);
+        List<WishList> wishLists = new ArrayList<>();
+        for (Client client : clients) {
+            WishList wishList = findByClientId(client.getId());
+            if (wishList != null)
+                wishLists.add(wishList);
+        }
+        return wishLists;
+    }
 
-    // Delete product
+    public WishList findByClientId(Long id) {
+        Optional<Client> optionalClient = clientService.findById(id);
+        if (optionalClient.isPresent()) {
+            List<WishList> wishListList = wishListRepository.findByClient(optionalClient.get());
+            if (!wishListList.isEmpty())
+                return wishListList.get(0);
+        }
+        return null;
+    }
 
-    // Find all products of the list
-
-    // Find product of the list by name
-
+    public void delete(WishList wishList) {
+        wishListRepository.delete(wishList);
+    }
 }
