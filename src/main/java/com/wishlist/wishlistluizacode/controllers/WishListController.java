753b79b912d664e59c4581a46d1e33c9f763c3a1
Carla-Lima <carlacarvalho93@hotmail.com>
2021-05-16 15:43:45 -0300

    add controller wishlist

--- src/main/java/com/wishlist/wishlistluizacode/controllers/WishListController.java
+++ src/main/java/com/wishlist/wishlistluizacode/controllers/WishListController.java
@@ -1,4 +1,36 @@
 package com.wishlist.wishlistluizacode.controllers;
 
+import com.wishlist.wishlistluizacode.entities.WishList;
+import com.wishlist.wishlistluizacode.repositories.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/wishlist")
 public class WishListController {
+
+    @Autowired
+    private WishListRepository wishListRepository;
+
+    //post wishlist
+    @PostMapping("/wishlistPost")
+    public WishList save(@RequestBody WishList wishList) {
+        return wishListRepository.save(wishList);
+    }
+
+    //get all wishlist
+    @GetMapping("/wishlistGetAll")
+    public ResponseEntity<List<WishList>> GetAll() {
+        return (ResponseEntity<List<WishList>>) wishListRepository.findAll();
+    }
+
+    //delete wishlist
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id) {
+        wishListRepository.deleteById(id);
+    }
 }
