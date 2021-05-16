8cca29d03a68a65c91efe64b95b75b73351c6758
Livia Silva <35042827+lsilva18@users.noreply.github.com>
2021-05-16 15:07:03 -0300

    Product controller

--- src/main/java/com/wishlist/wishlistluizacode/controllers/ProductController.java
+++ src/main/java/com/wishlist/wishlistluizacode/controllers/ProductController.java
@@ -1,4 +1,40 @@
 package com.wishlist.wishlistluizacode.controllers;
 
+
+import com.wishlist.wishlistluizacode.entities.Product;
+import com.wishlist.wishlistluizacode.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping("/product")
+    public Product save(@RequestBody Product product) {
+        return productService.save(product);
+    }
+
+    @GetMapping("/product")
+    public List<Product> findAlll() {
+        return productService.findAll();
+    }
+
+    @GetMapping("/product/{name}")
+    public List<Product> findByName(@PathVariable(value = "name") String name) {
+        return productService.findByName(name);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public void delete(@RequestBody Product product) {
+        productService.delete(product);
+    }
+
 }
+
