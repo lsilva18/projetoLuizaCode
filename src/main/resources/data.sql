27a9f864d795930ca6520c81292f44532c1246fd
Patricia Batalha <patibatalha@gmail.com>
2021-05-20 18:19:54 -0300

    fixing initial data load.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO Client (id, name, cpf) values (1, 'Carla', '1234561')
+INSERT IGNORE INTO Client (id, name, cpf) values (1, 'Carla', '1234561')
 
-INSERT INTO Client (id, name, cpf) values (2, 'Lara', '1234562')
+INSERT IGNORE INTO Client (id, name, cpf) values (2, 'Lara', '1234562')
 
-INSERT INTO Client (id, name, cpf) values (3, 'Lívia', '1234563')
+INSERT IGNORE INTO Client (id, name, cpf) values (3, 'Lívia', '1234563')
 
-INSERT INTO Client (id, name, cpf) values (4, 'Patrícia', '1234564')
+INSERT IGNORE INTO Client (id, name, cpf) values (4, 'Patrícia', '1234564')
 
-INSERT INTO Product (id, name, description) values (1, 'Aspirador Robô', 'Aspirador Robô')
+INSERT IGNORE INTO Product (id, name, description) values (1, 'Aspirador Robô', 'Aspirador Robô')
 
-INSERT INTO Product (id, name, description) values (2, 'Lava-Louça', 'Lava-Louça')
+INSERT IGNORE INTO Product (id, name, description) values (2, 'Lava-Louça', 'Lava-Louça')
